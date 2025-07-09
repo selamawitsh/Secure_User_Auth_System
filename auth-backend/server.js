c9e59d491af482beede3e837dc687aa72a3b93c7
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-07-09 04:32:58 +0300

    updateing server file

--- auth-backend/server.js
+++ auth-backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.route.js';
 
@@ -9,7 +10,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 
